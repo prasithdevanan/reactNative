57e915eb0f30103fa23047cd35d58905655e2e90
prasithdevanan <prasith4545@gmail.com>
2026-09-22 18:40:45 +0530

    create the filterstore

---
+++ store/filterStore.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+
+export type PropertyType = "appartment" | "house" | "villa" | "duplex" | "penthouse" | null;
+
+interface FilterState {
+    search: string;
+    type: PropertyType;
+    betrooms: number | null;
+    minPrice: number | null;
+    maxPrice: number | null;
+
+    setSearch: (search: string) => void;
+    setType: (type: PropertyType) => void;
+    setBetrooms: (betrooms: number | null) => void;
+    setMinPrice: (minPrice: number | null) => void;
+    setMaxPrice: (maxPrice: number | null) => void;
+
+    resetFilter: () => void;
+}
+
+export const useFilterStore = create<FilterState>((set) => ({
+    search: "",
+    type: null,
+    betrooms: null,
+    minPrice: null,
+    maxPrice: null,
+
+    setSearch: (search: string) => set({ search }),
+    setType: (type: PropertyType) => set({ type }),
+    setBetrooms: (betrooms: number | null) => set({ betrooms }),
+    setMinPrice: (minPrice: number | null) => set({ minPrice }),
+    setMaxPrice: (maxPrice: number | null) => set({ maxPrice }),
+
+    resetFilter: () => set({ search: "", type: null, betrooms: null, minPrice: null, maxPrice: null }),
+
+}));
